9b05aeebd18804ba9c727ad81c5a1bad7860ca38
Hugh Greenberg <hugegreenbug@gmail.com>
2016-02-08 18:37:56 -0700

    Adjusted the fling to scroll filter

--- src/fling_to_scroll_filter_interpreter.cc
+++ src/fling_to_scroll_filter_interpreter.cc
@@ -119,7 +119,7 @@ namespace gestures {
 		in_fling_ = 1;
 		two_finger_time_ = 0;
 		last_scroll_ = 0;
-		curve_duration_ = 0.005f * max_start_velocity;
+		curve_duration_ = 0.0034f * max_start_velocity;
 		vel_[0] = gesture.details.fling.vx;
 		vel_[1] = gesture.details.fling.vy;
 		start_time_ = gesture.start_time;
